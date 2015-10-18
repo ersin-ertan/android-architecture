d7fff5d5ee92c77d3ac747ac2de50f1d80f49b32
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-10-17 20:34:07 -0400

    Added: initial commit of standard dagger 2 application, activity, fragment template

---
+++ template00/src/main/java/com/nullcognition/template00/T.java
@@ -0,0 +1,6 @@
+package com.nullcognition.template00;
+// ersin 17/10/15 Copyright (c) 2015+ All rights reserved.
+
+
+public class T{
+}
---
+++ template00/src/main/java/com/nullcognition/template00/di/E.java
@@ -0,0 +1,6 @@
+package com.nullcognition.template00.di;
+// ersin 17/10/15 Copyright (c) 2015+ All rights reserved.
+
+
+public class E{
+}
