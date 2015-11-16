70254ab524a1bc0ac8caaf9567d1005941bf2b82
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-11-15 22:39:58 -0500

    Added: simple navi on rotation toast, with the listener

--- navi/src/main/java/com/nullcognition/navi/MainActivity.java
+++ navi/src/main/java/com/nullcognition/navi/MainActivity.java
@@ -1,13 +1,33 @@
 package com.nullcognition.navi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+import com.trello.navi.Event;
+import com.trello.navi.Listener;
+import com.trello.navi.NaviComponent;
+import com.trello.navi.component.support.NaviAppCompatActivity;
+
+public class MainActivity extends NaviAppCompatActivity{
+
+	public NaviComponent naviComponent = this;
+	public static int i;
+
+	public MainActivity(){
+		naviComponent.addListener(Event.PAUSE, new Listener<Void>(){
+			@Override public void call(final Void aVoid){
+
+				Toast.makeText(MainActivity.this, "navicomponent call" + ++i, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 	}
 }
+
